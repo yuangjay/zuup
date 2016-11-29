fb44f4aa1f0803fbb384dc09872e3756ac03bc14
Mehdi Abaakouk <sileht@sileht.net>
2016-11-29 08:11:14 +0100

    Update release script

--- releases.sh
+++ releases.sh
@@ -4,17 +4,22 @@ set -e
 set -x
 
 version=$1
+[ ! "$version" ] && version=$(python setup.py --version | sed 's/\.dev.*//')
 
 status=$(git status -sz)
 [ -z "$status" ] || false
 git checkout master
+tox -epy35,py27,pep8
 git push 
 git tag -s $version -m "Release version ${version}"
 git checkout $version
 git clean -fd
-rm -rf zuup.egg-info build dist
-tox -epep8,py27,py34
-tox -r -evenv python setup.py sdist bdist_wheel
+pbr_version=$(python setup.py --version)
+if [ "$version" != "$pbr_version" ]; then
+    echo "something goes wrong pbr version is different from the provided one. ($pbr_version != $version)"
+    exit 1
+fi
+python setup.py sdist bdist_wheel
 
 set +x
 echo
@@ -29,6 +34,7 @@ echo "uploading..."
 echo
 set -x
 
+read
 git push --tags
 twine upload -r pypi -s dist/zuup-${version}.tar.gz dist/zuup-${version}-py2.py3-none-any.whl
 git checkout master
